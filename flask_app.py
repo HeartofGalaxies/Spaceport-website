912b5dda56bf70e21b5d24e375cca414ae02c49a
Kayleigh Roberts <kayleigh.ruth.roberts@gmail.com>
2018-12-14 14:34:50 +0000

    deleted extra images and added subscribe button action

--- flask_app.py
+++ flask_app.py
@@ -7,3 +7,20 @@ def main():
 
 if __name__ == '__main__':
     app.run()
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    inputEmail = request.form.get('inputEmail')
+    if "@" in inputEmail:
+        return render_template("email_error.html")
+    else:
+        return render_template("net_error.html")
+
+"""@app.route('/process_inputs', methods=['POST'])
+def process_inputs():
+    name = request.form.get('input_name', '')
+    dropdown = request.form.get('input_dropdown', '')
+    select = request.form.get('input_select', '')
+    freeform = request.form.get('input_freeform', '')
+    return render_template("main_page.html", input_data=dropdown,
+                           output="You're a wizard %s." % name)"""
--- static/js/grayscale.og..js
+++ static/js/grayscale.og..js
@@ -40,15 +40,3 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
-
-$(window).load(function () {
-    $(".trigger_popup_fricc").click(function(){
-       $('.hover_bkgr_fricc').show();
-    });
-    $('.hover_bkgr_fricc').click(function(){
-        $('.hover_bkgr_fricc').hide();
-    });
-    $('.popupCloseButton').click(function(){
-        $('.hover_bkgr_fricc').hide();
-    });
-});
